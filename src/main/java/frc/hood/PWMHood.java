ee07dcb16ebf57ca7ecd55129427ecda02016081
Nikki Chong <104547798+usagi1248@users.noreply.github.com>
2022-05-01 13:42:08 -0700

    Add files via upload
    womp womp

---
+++ src/main/java/frc/hood/PWMHood.java
@@ -0,0 +1,18 @@
+package hood;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+
+public class PWMHood extends PWMVictorSPX {
+    private double movementSpeed = 0.2;
+
+    public PWMHood(int channel, double speed) { super(channel); movementSpeed=speed; } // constructor for custom motor speed
+    public PWMHood(int channel) { super(channel); } // constructor for default motor speed
+
+    public void move(double units){
+        
+    }
+    public void moveTo(double position){
+
+    }
+
+}
