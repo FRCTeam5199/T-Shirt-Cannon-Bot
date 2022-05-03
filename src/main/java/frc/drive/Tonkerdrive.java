b3350b8ba6757d986fe93f8af9d75e62be40338c
oongaboongadoonga <jreidel8@gmail.com>
2022-05-02 19:46:21 -0700

    CTR Firmware

--- src/main/java/frc/drive/Tonkerdrive.java
+++ src/main/java/frc/drive/Tonkerdrive.java
@@ -1,7 +1,7 @@
 package frc.drive;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.PWMVictorSPX;
 import frc.controllers.XboxController;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
