8a7a2e38112c41cd267026b7563c0d270042afb2
oongaboongadoonga <jreidel8@gmail.com>
2022-05-02 19:42:03 -0700

    Update Tonkerdrive.java

--- src/main/java/frc/drive/Tonkerdrive.java
+++ src/main/java/frc/drive/Tonkerdrive.java
@@ -1,7 +1,18 @@
 package frc.drive;
 
-import com.ctre.phoenix.
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import frc.controllers.XboxController;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+
+
 
 public class Tonkerdrive {
+    private PWMVictorSPX left1;
+
+    
     
 }
