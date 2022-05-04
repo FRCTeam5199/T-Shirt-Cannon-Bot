783ac4d62388065e74fbf7ff4b88f541deeb706c
oongaboongadoonga <jreidel8@gmail.com>
2022-05-03 21:19:54 -0700

    Update Tonkerdrive.java

--- src/main/java/frc/drive/Tonkerdrive.java
+++ src/main/java/frc/drive/Tonkerdrive.java
@@ -1,13 +1,15 @@
 package frc.drive; 
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-import com.ctre.phoenix.motorcontrol.IMotorController;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
 
 import frc.controllers.XboxController;
+
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.drive.*;
+import edu.wpi.first.wpilibj.*;
 
 import java.util.*;
 
@@ -17,27 +19,24 @@ import java.util.*;
 
 public class Tonkerdrive {
     //defining motors on bot
-    private static VictorSPX left1;
-    private static VictorSPX left2;
-    private static VictorSPX right1;
-    private static VictorSPX right2;
+    static VictorSPX left1 = new VictorSPX(0);
+    static VictorSPX left2 = new VictorSPX(1);
+    static VictorSPX right1 = new VictorSPX(2);
+    static VictorSPX right2 = new VictorSPX(3);
 
-    //Too dumb to think of a better way to combine motors
-    public class LeftGroupMotorController {
-        List<IMotorController> left = new ArrayList<IMotorController>();
-    }
-    public class RightGroupMotorController {
-        List<IMotorController> right = new ArrayList<IMotorController>();
+    private void add(VictorSPX motors) {
+        motors.set(VictorSPXControlMode.PercentOutput, .05);
     }
 
-    public static void combineL(IMotorController left){
-        ((List<IMotorController>) left).add(left1);
-        ((List<IMotorController>) left).add(left2);
-    }
 
-    public static void combineR (IMotorController right){
-        ((List<IMotorController>) right).add(right1);
-        ((List<IMotorController>) right).add(right2);
-    }
+    GroupMotorControllers left = new GroupMotorControllers();
 
+    public static void LeftGroup(IMotorController left){
+        ((Tonkerdrive) left).add(left1);
+        ((Tonkerdrive) left).add(left2);
+    }
+    public static void RightGroup(IMotorController right){
+        ((Tonkerdrive) right).add(right1);
+        ((Tonkerdrive) right).add(right2);
+    }
 }
