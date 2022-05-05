477445aada278ccf4f7e9d9558666b195a598c67
Nikki Chong <chong.jeffrie@gmail.com>
2022-05-04 19:36:55 -0700

    two bros. sittin in a hot tub. 5 feet apart cuz they're not gay
    a

--- src/main/java/frc/tilt/PWMHood.java
+++ src/main/java/frc/tilt/PWMHood.java
@@ -1,18 +1,30 @@
 package frc.tilt;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import com.ctre.phoenix.*;
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.*;
 
-public class PWMHood extends PWMVictorSPX {
-    private double movementSpeed = 0.2;
+public class PWMHood{
+    VictorSPX motor;
+    double multiplier = 0.1;
 
-    public PWMHood(int channel, double speed) { super(channel); movementSpeed=speed; } // constructor for custom motor speed
-    public PWMHood(int channel) { super(channel); } // constructor for default motor speed
-
-    public void tilt(double units){
-        
+    public PWMHood(int deviceNumber) { motor = new VictorSPX(deviceNumber); initMotorSpeed(); } // constructor for defualt motor output
+    public PWMHood(int deviceNumber, double nMultiplier) { motor = new VictorSPX(deviceNumber); multiplier = nMultiplier; initMotorSpeed(); } // constructor for custom motor speed 
+    
+    private void initMotorSpeed(){ // set motor speed from multiplier
+        if( !(motor.configPeakOutputForward(multiplier).equals(ErrorCode.OK) && motor.configPeakOutputReverse(multiplier).equals(ErrorCode.OK)) ){
+            System.out.println("this robot does not vibe with the victor motors");
+        } else {
+            System.out.println("hood motors good to go");
+        }
     }
-    public void tiltTo(double position){
 
+    public void setToAngle(double angle){
+        motor.set(ControlMode.Position, angle * (4096.0 / 360.0)); // convert from angle to the 4096-units-per-rotation
+    }
+
+    public double getPosition() {
+        return motor.getSelectedSensorPosition() * (360.0  / 4096.0); // and vice versa here
     }
 
 }
