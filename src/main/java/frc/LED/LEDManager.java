66b821927523068b404405a817ff4464f4e02ec8
oongaboongadoonga <jreidel8@gmail.com>
2022-05-13 13:56:27 -0700

    Update LEDManager.java

--- src/main/java/frc/LED/LEDManager.java
+++ src/main/java/frc/LED/LEDManager.java
@@ -4,15 +4,15 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 public class LEDManager {
-    AddressableLED LEDS = new AddressableLED(1);
-    
+    AddressableLED strip1 = new AddressableLED(1);
+    AddressableLED strip2 = new AddressableLED(1);
+    AddressableLED strip3 = new AddressableLED(1);
+    AddressableLED strip4 = new AddressableLED(1);
 
     public void LEDModeCapoRally() {
-
-
-
-        
-        
+        strip1.setLength(500);
+        strip2.setLength(500);
+        strip3.setLength(500);
+        strip4.setLength(500);
     }
-
 }
