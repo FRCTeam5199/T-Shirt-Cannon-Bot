5ea9f4de0bb6ef0cd943f73c2500156a2620a965
oongaboongadoonga <jreidel8@gmail.com>
2022-05-16 19:45:17 -0700

    ererererrer

--- src/main/java/frc/LED/LEDManager.java
+++ src/main/java/frc/LED/LEDManager.java
@@ -2,17 +2,21 @@ package frc.LED;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LEDManager {
-    AddressableLED LEDRGB  = new AddressableLED(1);
-    AddressableLEDBuffer LEDBUFFER = new AddressableLEDBuffer(500);
+    static AddressableLED LEDRGB  = new AddressableLED(1);
+    static AddressableLEDBuffer LEDBUFFER = new AddressableLEDBuffer(500);
+    Timer timer = new Timer();
+    boolean swap = false;
 
-    public void Init() {
+    public static void Init() {
+        timer.start();
         LEDRGB.setLength(LEDBUFFER.getLength());
         LEDRGB.setData(LEDBUFFER);
     }
 
-    public void test() {
+    public static void test() {
         int[][] Rainer = new int[][]{{255,0,0},{245,167,12}, {255,248,9}, {0,255,0},{0,255,255}, {0,0,255}, {128,0,255}, {255,0,140}};
         for (int Rainbow = 0; Rainbow < LEDBUFFER.getLength();Rainbow++){
             LEDBUFFER.setRGB(Rainbow, Rainer[Rainbow%8][0], Rainer[Rainbow%8][1], Rainer[Rainbow%8][2]);
