c92215ec873acd0d833de55d6ff826dbca5fd0eb
oongaboongadoonga <jreidel8@gmail.com>
2022-05-16 19:24:18 -0700

    Update LEDManager.java

--- src/main/java/frc/LED/LEDManager.java
+++ src/main/java/frc/LED/LEDManager.java
@@ -10,7 +10,21 @@ public class LEDManager {
     public void Init() {
         LEDRGB.setLength(LEDBUFFER.getLength());
         LEDRGB.setData(LEDBUFFER);
-
     }
 
+    public void test() {
+        int[][] Rainer = new int[][]{{255,0,0},{245,167,12}, {255,248,9}, {0,255,0},{0,255,255}, {0,0,255}, {128,0,255}, {255,0,140}};
+        for (int Rainbow = 0; Rainbow < LEDBUFFER.getLength();Rainbow++){
+            LEDBUFFER.setRGB(Rainbow, Rainer[Rainbow%8][0], Rainer[Rainbow%8][1], Rainer[Rainbow%8][2]);
+        }
+    }
+
+    public void DOLPHIN(){
+        for (int blue = 0; blue < LEDBUFFER.getLength(); blue++) {
+            LEDBUFFER.setRGB(blue ,25, 19, 210);
+        }
+        for (int grey = 0; grey < LEDBUFFER.getLength(); grey++){
+            LEDBUFFER.setRGB(grey, 139,139,141);
+        }
+    }
 }
