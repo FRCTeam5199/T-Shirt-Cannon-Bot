8c1aca3cdecf98899e9bbcf9fe9869f74d12e4e7
Ethan-Schwartzman <ethancschwartzman@gmail.com>
2022-05-16 19:53:18 -0700

    LEDs

--- src/main/java/frc/LED/LEDManager.java
+++ src/main/java/frc/LED/LEDManager.java
@@ -10,7 +10,7 @@ public class LEDManager {
     Timer timer = new Timer();
     boolean swap = false;
 
-    public static void Init() {
+    public void Init() {
         timer.start();
         LEDRGB.setLength(LEDBUFFER.getLength());
         LEDRGB.setData(LEDBUFFER);
@@ -29,6 +29,19 @@ public class LEDManager {
         }
         for (int grey = 0; grey < LEDBUFFER.getLength(); grey++){
             LEDBUFFER.setRGB(grey, 139,139,141);
+        }
+    }
+
+    
+    public void capoLEDMode() {
+        //Each second, alternates between black and yellow & yellow and black LEDs
+        if (timer.get() >= 1) {
+            for (int i = 0; i < LEDBUFFER.getLength(); i++) {
+                int color = ((i%2 == 0) ^ (swap))? 0 : 255; 
+                LEDBUFFER.setRGB(i, color, color, 0);
+            }
+            swap = !swap;
+            timer.reset();
         }
     }
 }
