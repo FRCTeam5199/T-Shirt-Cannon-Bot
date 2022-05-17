021fa742451c01d83fdf01726345beba5ef2a281
oongaboongadoonga <jreidel8@gmail.com>
2022-05-16 18:32:35 -0700

    Update LEDManager.java

--- src/main/java/frc/LED/LEDManager.java
+++ src/main/java/frc/LED/LEDManager.java
@@ -4,12 +4,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 
 public class LEDManager {
+    AddressableLED LEDRGB  = new AddressableLED(1);
+    AddressableLEDBuffer LEDBUFFER = new AddressableLEDBuffer(500);
 
-    public void init() {
-        
-
-    }
-
-    public void LEDModeCapoRally() {
-    }
 }
