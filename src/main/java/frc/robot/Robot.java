175c873e72e03e8973d73b21a3bbcd0647bd7eba
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2023-10-04 19:04:31 -0700

    created the template for command base

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,253 +1,103 @@
 // Copyright (c) FIRST and other WPILib contributors.
-
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import frc.Constants;
-import frc.LED.LEDManager;
-//import edu.wpi.first.wpilibj.XboxController;
-import frc.controllers.ControlPanel;
-import frc.controllers.XboxController;
-import frc.drive.Tonkerdrive;
-import frc.hood.TiltHood;
-import frc.misc.ISubsystem;
-
-import java.util.ArrayList;
-//import com.ctre.phoenix.*;
-//import com.ctre.phoenix.motorcontrol.*;
-//import com.ctre.phoenix.motorcontrol.can.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * The VM is configured to automatically run this class, and to call the methods
- * corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-    public static XboxController stick1;
-    public static ControlPanel controlPanel;
-    public static Tonkerdrive drive = new Tonkerdrive();
-    public static final ArrayList<ISubsystem> subsystems = new ArrayList<>();
+  private Command m_autonomousCommand;
 
-    /**
-     * This method is run when the robot is first started up and should be used for
-     * any
-     * initialization code.
-     */
+  private RobotContainer m_robotContainer;
 
-    public class WarningLed implements Runnable{
-        private Thread t;
-        private String Led;
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // autonomous chooser on the dashboard.
+    m_robotContainer = new RobotContainer();
+  }
 
-        WarningLed(String warn){
-            Led = warn;
-        }
-        public void run(){
-            try{
-                while(controlPanel.safetySwitch()) {
-                ledManager.safetymode();
-                }
-            }catch(IllegalAccessError | InterruptedException error){
-                System.out.println(" ");
-            }
-        }
-        public void start(){
-            if(t == null){
-                t = new Thread(this, Led);
-                t.start();
-            }
-        }
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
+    CommandScheduler.getInstance().run();
+  }
+
+  /** This function is called once each time the robot enters Disabled mode. */
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
+  }
 
-    //Variables
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {}
 
-    // Control
-    public static boolean driveEnabled = true;
-    private boolean hoodEnabled = true;
-
-    // Inputs
-    XboxController xboxController;
-
-    // hood
-    TiltHood hood;
-
-    // LED Manager
-    LEDManager ledManager = new LEDManager();
-
-    // Pressure sensor
-    AnalogInput pressureSensor;
-    double chargePSI;
-
-    //Compressor
-    private boolean compressorEnabled = false;
-    //Compressor compressor = new Compressor(0, PneumaticsModuleType.REVPH);
-
-    //pre fireing
-    private boolean isPrefiring = false;
-    private boolean prefiringStrobe = false;
-
-    //Safety
-    private boolean safetyMode = false;
-
-    //Main
-    @Override
-
-    //Robot Init
-    public void robotInit() {
-        System.out.println("initializing");
-        xboxController = new XboxController(Constants.XBOX_CONTROLLER_PORT);
-        stick1 = new XboxController(Constants.XBOX_CONTROLLER_PORT);
-        drive.driveInit();
-        // simpleWidget = Shuffleboard.getTab("Tab").add("Title", "value");
-        controlPanel = new ControlPanel(Constants.CONTROL_PANEL_PORT);
-
-        hood = new TiltHood(Constants.TILT_MOTOR_ID, Constants.SHOOTER_SOLENOID_ID_1, Constants.SHOOTER_SOLENOID_ID_2, Constants.RESERVE_SOLENOID_ID);
-        pressureSensor = new AnalogInput(0);
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
+  }
 
-    @Override
-    public void robotPeriodic() {
-    }
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
 
-    @Override
-    public void autonomousInit() {
-    }
+  @Override
+  public void testInit() {
+    // Cancels all running commands at the start of test mode.
+    CommandScheduler.getInstance().cancelAll();
+  }
 
-    @Override
-    public void autonomousPeriodic() {
-    }
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 
-    //Tele-op init
-    @Override
-    public void teleopInit() {
-        System.out.println("initalizing");
-        stick1 = new XboxController(Constants.XBOX_CONTROLLER_PORT);
-        hood.resetShooter();
-        ledManager.yellow();
-        System.out.println("done initalizing");
-    }
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
 
-    //Tele-op periodic
-    @Override
-    public void teleopPeriodic() {
-
-        WarningLed warning = new WarningLed("orange");
-
-        //ledManager.capoLEDMode();TODO
-
-        if (/*controlPanel.killSwitch()TODO*/ true) {
-
-            // Drive
-            drive.Teleop();
-            
-            /*TODO
-            if(stick1.getButton(6)) {
-                safetyMode = !safetyMode;
-                System.out.println("safety is " + safetyMode);
-                Timer.delay(1);
-            }
-            */
-            
-            //Toggle compressor with Left-bumper
-            if(xboxController.getButton(5) || stick1.getButton(5)) {
-                System.out.println("toggling compressor");
-                if(compressorEnabled == true) {
-                    TiltHood.compressor.enableDigital();
-                }
-                else {
-                    TiltHood.compressor.disable();
-                }
-                
-                compressorEnabled = !compressorEnabled;
-                
-                Timer.delay(1);
-            }
-        
-
-
-            // Shooter
-            // getVoltage returns a voltage between 0 and 5v
-            // The REV website states that with this model of sensor 5v = 200 psi
-            if (true/*safetyMode8*//*controlPanel.safetySwitch()TODO*/) {
-                warning.start();
-                chargePSI = pressureSensor.getVoltage() * 40;
-
-                //Fire
-                if ((xboxController.getButton(4) || controlPanel.shoot()) && Constants.SHOOTER_ENABLED /*&& chargePSI >= 60 TODO*/) {
-                    isPrefiring = false;
-                    prefiringStrobe = false;
-                    hood.closeReserve();
-                    hood.fireShot();
-                }
-                /*
-                } else {
-                    hood.resetShooter();
-                    //hood.closeReserve();
-                    if (chargePSI < 90) {
-                        hood.openReserve();
-                    } else {
-                        hood.closeReserve();
-                    }
-                }
-                */
-                //Pre-fire strobe
-                else if(xboxController.getButton(3) || stick1.getButton(3) || controlPanel.safetySwitch()) {
-                    prefiringStrobe = !prefiringStrobe;
-                    Timer.delay(0.5);
-                }
-                //Pre-fire
-                else if(xboxController.getButton(2) || stick1.getButton(2) || controlPanel.safetySwitch()) {
-                    isPrefiring = !isPrefiring;
-                    Timer.delay(0.5);
-                }
-                else if(isPrefiring == true) {
-                    hood.openReserve();
-                    if(prefiringStrobe) {
-                        ledManager.yellow();
-                        Timer.delay(0.15);
-                        ledManager.test();
-                        Timer.delay(0.15);
-                    }
-                    else {
-                        ledManager.test();
-                    }
-                    System.out.println("pre-firing");
-                }
-                else {
-                    hood.closeAll();
-                    ledManager.Rainbow3();
-                }
-
-                //Disable compressor if PSI above 120
-                if(chargePSI >= 120) {
-                    TiltHood.compressor.disable();
-                    compressorEnabled = false;
-                }
-            }
-        }
-        hood.logMotorRotations();
-    }
-
-    @Override
-    public void disabledInit() {
-    }
-
-    @Override
-    public void disabledPeriodic() {
-    }
-
-    @Override
-    public void testInit() {
-    }
-
-    @Override
-    public void testPeriodic() {
-    }
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
 }
