da9594d11df8e54c117308dfbc2c9e96a8659d8e
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2023-10-09 19:38:13 -0700

    added tonkerdrive

--- src/main/java/frc/robot/drive/Tonkerdrive.java
+++ src/main/java/frc/robot/drive/Tonkerdrive.java
@@ -5,16 +5,18 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class Tonkerdrive {
+public class Tonkerdrive extends SubsystemBase {
     public static VictorSPX left1;
     public static VictorSPX left2;
     public static VictorSPX right1;
     public static VictorSPX right2;
+    CommandXboxController xboxController = new CommandXboxController(Constants.XBOX_CONTROLLER_PORT);
 
-    Tonkerdrive() {
+    public Tonkerdrive() {
         left1 = new VictorSPX(Constants.LEFT_1_DEVICE_ID);
         left2 = new VictorSPX(Constants.LEFT_2_DEVICE_ID);
         right1 = new VictorSPX(Constants.RIGHT_1_DEVICE_ID);
@@ -26,9 +28,17 @@ public class Tonkerdrive {
         right1.setInverted(Constants.IS_INVERTED);
     }
 
-    public Command drive(XboxController xboxController) {
-    return this.runOnce(() -> {
-        left1.set(ControlMode.PercentOutput, ((xboxController.getLeftY() + (xboxController.getRXAxis() * Constants.TURN_FACTOR)) * Constants.VOLTAGE_MULT) * Constants.MAX_SPEED_PERCENT);
-        right1.set(ControlMode.PercentOutput, ((-xboxController.getLeftY() + (xboxController.getRXAxis() * Constants.TURN_FACTOR)) * Constants.VOLTAGE_MULT) * Constants.MAX_SPEED_PERCENT);
-    });
+    public Command drive() {
+        return this.runOnce(() -> {
+            left1.set(ControlMode.PercentOutput, ((xboxController.getLeftY() + (xboxController.getRightX() * Constants.TURN_FACTOR)) * Constants.VOLTAGE_MULT) * Constants.MAX_SPEED_PERCENT);
+            right1.set(ControlMode.PercentOutput, ((-xboxController.getLeftY() + (xboxController.getRightX() * Constants.TURN_FACTOR)) * Constants.VOLTAGE_MULT) * Constants.MAX_SPEED_PERCENT);
+        });
+    }
+
+    @Override
+    public void periodic() {
+        if (xboxController.getLeftY() > 0.01 || xboxController.getRightX() > 0.01) {
+            drive();
+        }
+    }
 }
