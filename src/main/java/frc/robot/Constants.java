da3fe5e12b0af43f0fb9d9bb1c0429e047e9981f
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2023-10-10 17:57:17 -0700

    cleaned up code more

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,14 +9,10 @@ public class Constants {
 
     public static final double TURN_FACTOR = 0.5;
     public static final double VOLTAGE_MULT = 9.2;
-    public static final boolean IS_INVERTED = false;
     public static final double MAX_SPEED_PERCENT = 0.1;
     public static final int XBOX_CONTROLLER_PORT = 0;
-    public static final int PANEL_OF_CONTROL_PORT = 1;
-    public static final int CONTROL_PANEL_PORT = 2;
     public static final int LED_PORT = 0;
     public static final int LED_LENGTH = 89;
-    public static final boolean SHOOTER_ENABLED = true;
     public static final int TILT_MOTOR_ID = 7;
     public static final int SHOOTER_SOLENOID_ID_1 = 3;
     public static final int SHOOTER_SOLENOID_ID_2 = 1;
@@ -25,11 +21,6 @@ public class Constants {
     //Adjust these later
     public static final double[] ANGLE_POSITIONS = {30.0, 45.0, 60.0}; // change for different angle positions
     public static final int COMPRESSOR_ID = 1;
-    public static final int OVERHEAT_THRESHOLD = 80;
-    public static final boolean ENABLE_OVERHEAT_DETECTION = false;
-    public static final boolean SECOND_TRY = true;
-
-    //Set these values later
     public static final double TILT_HOOD_P = 0;
     public static final double TILT_HOOD_I = 0;
     public static final double TILT_HOOD_D = 0;
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,6 @@ import frc.robot.drive.Tonkerdrive;
 import frc.robot.subsystems.TiltHood;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
