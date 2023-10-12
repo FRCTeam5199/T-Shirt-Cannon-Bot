7fdbb7182abc03cba1053647ae3461ca35f9f0c0
Azelf137 <ryzen0137@gmail.com>
2023-10-11 19:34:37 -0700

    Moving LED to CommandBase

--- src/main/java/frc/robot/LED/LED.java
+++ src/main/java/frc/robot/LED/LED.java
@@ -42,67 +42,78 @@ public class LED extends SubsystemBase{
         });
     }
 
-    public void yellow() {
-        for(var i = 0; i < LEDBUFFER.getLength(); i++) {
-            LEDBUFFER.setRGB(i, 255, 255, 0);
-        }
-        LEDRGB.setLength(LEDBUFFER.getLength());
-        LEDRGB.setData(LEDBUFFER);
-        LEDRGB.start();
-    }
-
-    public void Rainbow() {
-        int[][] Rainer = new int[][]{{255, 0, 0}, {245, 167, 12}, {255, 248, 9}, {0, 255, 0}, {0, 255, 255}, {0, 0, 255}, {128, 0, 255}, {255, 0, 140}};
-        for (int Rainbow = 0; Rainbow < LEDBUFFER.getLength(); Rainbow++) {
-            LEDBUFFER.setRGB(Rainbow, Rainer[Rainbow % 8][0], Rainer[Rainbow % 8][1], Rainer[Rainbow % 8][2]);
-            //LEDRGB.setData(LEDBUFFER);
+    public Command yellow() {
+        return this.runOnce(() -> {
+            for(var i = 0; i < LEDBUFFER.getLength(); i++) {
+                LEDBUFFER.setRGB(i, 255, 255, 0);
+            }
             LEDRGB.setLength(LEDBUFFER.getLength());
             LEDRGB.setData(LEDBUFFER);
             LEDRGB.start();
-        }
+        });
+    }
+
+    public void Rainbow() {
+        return this.runOnce(() -> {
+            int[][] Rainer = new int[][]{{255, 0, 0}, {245, 167, 12}, {255, 248, 9}, {0, 255, 0}, {0, 255, 255}, {0, 0, 255}, {128, 0, 255}, {255, 0, 140}};
+            for (int Rainbow = 0; Rainbow < LEDBUFFER.getLength(); Rainbow++) {
+                LEDBUFFER.setRGB(Rainbow, Rainer[Rainbow % 8][0], Rainer[Rainbow % 8][1], Rainer[Rainbow % 8][2]);
+                //LEDRGB.setData(LEDBUFFER);
+                LEDRGB.setLength(LEDBUFFER.getLength());
+                LEDRGB.setData(LEDBUFFER);
+                LEDRGB.start();
+            }
         //LEDRGB.setData(LEDBUFFER);
+        });
     }
 
     public void Rainbow2() {
-        for(var i = 0; i < LEDBUFFER.getLength(); i++) {
-            final var hue = (firstPixelHue + (i * 180 / LEDBUFFER.getLength())) % 180;
-            LEDBUFFER.setHSV(i, hue, 255, 128);
-        }
-        firstPixelHue += 3;
-        firstPixelHue %= 180;
+        return this.runOnce(() -> {
+            for(var i = 0; i < LEDBUFFER.getLength(); i++) {
+                final var hue = (firstPixelHue + (i * 180 / LEDBUFFER.getLength())) % 180;
+                LEDBUFFER.setHSV(i, hue, 255, 128);
+            }
+            firstPixelHue += 3;
+            firstPixelHue %= 180;
     }
 
     public void Rainbow3() {
-        Rainbow2();
-        LEDRGB.setData(LEDBUFFER);
+        return this.runOnce(() -> {
+            Rainbow2();
+            LEDRGB.setData(LEDBUFFER);
+        });
     }
 
     public void DOLPHIN() {
-        for (int blue = 0; blue < LEDBUFFER.getLength(); blue++) {
-            LEDBUFFER.setRGB(blue, 25, 19, 210);
-        }
-        for (int grey = 0; grey < LEDBUFFER.getLength(); grey++) {
-            LEDBUFFER.setRGB(grey, 139, 139, 141);
+        return this.runOnce(() -> {
+            for (int blue = 0; blue < LEDBUFFER.getLength(); blue++) {
+                LEDBUFFER.setRGB(blue, 25, 19, 210);
+            }
+            for (int grey = 0; grey < LEDBUFFER.getLength(); grey++) {
+                LEDBUFFER.setRGB(grey, 139, 139, 141);
+                //LEDRGB.setData(LEDBUFFER);
+            }
             //LEDRGB.setData(LEDBUFFER);
-        }
-        //LEDRGB.setData(LEDBUFFER);
+        });
     }
 
 
     public void capoLEDMode() {
-        //Each second, alternates between black and yellow & yellow and black LEDs
-        if (timer.get() >= 1) {
-            for (int i = 0; i < LEDBUFFER.getLength(); i++) {
-                int color = ((i % 2 == 0) ^ (swap)) ? 0 : 255;
-                LEDBUFFER.setRGB(i, color, color, 0);
+        return this.runOnce(() -> {
+            //Each second, alternates between black and yellow & yellow and black LEDs
+            if (timer.get() >= 1) {
+                for (int i = 0; i < LEDBUFFER.getLength(); i++) {
+                    int color = ((i % 2 == 0) ^ (swap)) ? 0 : 255;
+                    LEDBUFFER.setRGB(i, color, color, 0);
+                }
+                swap = !swap;
+                timer.reset();
+                LEDRGB.setLength(LEDBUFFER.getLength());
+                LEDRGB.setData(LEDBUFFER);
+                LEDRGB.start();
+                //LEDRGB.setData(LEDBUFFER);
             }
-            swap = !swap;
-            timer.reset();
-            LEDRGB.setLength(LEDBUFFER.getLength());
-            LEDRGB.setData(LEDBUFFER);
-            LEDRGB.start();
             //LEDRGB.setData(LEDBUFFER);
-        }
-        //LEDRGB.setData(LEDBUFFER);
+        });
     }
 }
